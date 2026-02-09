8e91e22cfd160338672296e96a0ada999d69449f
Frivolimous <moshe.jeremy@gmail.com>
2026-02-09 10:41:56 +0200

    bigger skew

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 Windwarp
 
 TODO
-Glitch jumping and climbing... (fixed???);
-Redo collision system velocity based
-Refactor movement system
-
 Hands and animated
 Animate head
 Split body in half and animate
 
 Block textures
-Stroke around Blocks
 Player textures
 
 Display config values
--- src/engine/Objects/PlayerSprite.ts
+++ src/engine/Objects/PlayerSprite.ts
@@ -6,172 +6,234 @@ import { Facade } from "../../main";
 import { GameCanvas } from "./GameCanvas";
 
 export class PlayerSprite extends PIXI.Container {
-    public keys = {
-        down: false,
-        up: false,
-        left: false,
-        right: false,
-        holdUp: false,
-        jetpack: false,
-    };
+  public keys = {
+    down: false,
+    up: false,
+    left: false,
+    right: false,
+    holdUp: false,
+    jetpack: false,
+  };
 
-    public doubleJumpsRemaining = 1;
+  public doubleJumpsRemaining = 1;
 
-    public wallGrabsRemaining = 1;
+  public wallGrabsRemaining = 1;
 
-    public movementState: MovementState = 'idle';
+  public movementState: MovementState = 'idle';
 
-    public vX = 0;
-    public vY = 0;
+  public vX = 0;
+  public vY = 0;
 
-    public bounceTime = 0;
-    public rollTime = 0;
-    public maxRollTime = 100;
-    public grabTime = 0;
-    public landTime = 0;
+  public bounceTime = 0;
+  public rollTime = 0;
+  public maxRollTime = 100;
+  public grabTime = 0;
+  public landTime = 0;
 
-    public desiredSkew = 0;
-    public desiredVStretch = 0;
-    private skewMult = -0.02;
-    private vStretchMult = 0.05;
-    private maxVStretch = 0.2;
-    private animationSpeed = 0.2;
+  public desiredSkew = 0;
+  public desiredVStretch = 0;
+  private skewMult = -0.04;
+  private vStretchMult = 0.1;
+  private maxVStretch = 0.2;
+  private animationSpeed = 0.2;
 
-    private maxStepDelay = 10;
-    private stepDelay = 0;
+  private maxStepDelay = 10;
+  private stepDelay = 0;
 
-    private view = new PIXI.Graphics();
-    public collider = new PIXI.Rectangle();
+  private handsTick = 0;
+  private handsTime = 40;
 
-    constructor(width: number, height: number) {
-        super();
+  private view = new PIXI.Graphics();
+  private leftHand = new PIXI.Graphics();
+  private rightHand = new PIXI.Graphics();
+  public collider = new PIXI.Rectangle();
 
-        this.collider.set(0, 0, width, height);
+  constructor(width: number, height: number) {
+    super();
 
-        this.addChild(this.view);
+    this.collider.set(0, 0, width, height);
 
-        this.drawPlayer();
+    this.addChild(this.view);
+    // this.addChild(this.leftHand, this.rightHand);
+
+    this.drawPlayer();
+  }
+
+  getMidPoint() {
+    return new PIXI.Point(this.x + this.collider.width / 2, this.y + this.collider.height / 2);
+  }
+
+  getFootPoint() {
+    return new PIXI.Point(this.x + this.collider.width / 2, this.y + this.collider.height);
+  }
+
+  drawPlayer() {
+    this.view.ellipse(this.collider.width / 2, this.collider.height / 2, this.collider.width / 2, this.collider.height / 2);
+    this.view.fill(0xff6633);
+    this.view.stroke({ width: 2, color: 0 });
+    this.view.circle(this.collider.width / 2, this.collider.width * 0.8, this.collider.width * 0.8);
+    this.view.fill(0xee1144);
+    this.view.stroke({ width: 2, color: 0 });
+    this.leftHand.circle(0, 0, this.collider.width * 0.3).fill(0xff6633).stroke({ width: 2, color: 0 });
+    this.rightHand.circle(0, 0, this.collider.width * 0.3).fill(0xff6633).stroke({ width: 2, color: 0 });
+  }
+
+  setMovementState(state: MovementState) {
+    if (state === 'ascending') {
+      Firework.makeExplosion(this.parent, _.defaults(this.getFootPoint(), STEP_PARTICLE));
+    } else if ((this.movementState === 'falling' || this.movementState === 'diving') && state === 'walking') {
+      Firework.makeExplosion(this.parent, _.defaults(this.getFootPoint(), STEP_PARTICLE));
+    }
+    this.movementState = state;
+    GameEvents.ACTIVITY_LOG.publish({ slug: 'PLAYER_STATE', text: state });
+  }
+
+  public getCollider(state?: MovementState) {
+    state = state || this.movementState;
+    if (state === 'crouching' || state === 'crawling' || state === 'rolling' || state === 'climbing-left' || state === 'climbing-right') {
+      return new PIXI.Rectangle(this.x, this.y + this.collider.height / 2, this.collider.width, this.collider.height / 2);
+    }
+    return new PIXI.Rectangle(this.x, this.y, this.collider.width, this.collider.height);
+  }
+
+  public getTopCollider() {
+    return new PIXI.Rectangle(this.x, this.y, this.collider.width, this.collider.height / 4);
+  }
+
+  updateView() {
+    this.stepDelay--;
+    if (this.stepDelay <= 0) {
+      this.stepDelay = this.maxStepDelay;
+      if (this.movementState === 'walking' || this.movementState === 'rolling') {
+        // {x: this.x + this.collider.width / 2, y: this.y + this.collider.height}
+        STEP_PARTICLE.x = this.x + this.collider.width / 2;
+        STEP_PARTICLE.y = this.y + this.collider.height;
+
+        Firework.makeExplosion(this.parent, _.defaults(this.getFootPoint(), STEP_PARTICLE));
+      }
     }
 
-    getMidPoint() {
-        return new PIXI.Point(this.x + this.collider.width / 2, this.y + this.collider.height / 2);
+    // this.updateHands();
+
+    if (this.movementState === 'jetpacking') {
+      Firework.makeExplosion(Facade.gamePage.canvas.layers[GameCanvas.OBJECTS], _.defaults(this.getMidPoint(), { count: 1, tint: 0xffcc66, mag_min: 1, mag_max: 2 }));
     }
 
-    getFootPoint() {
-        return new PIXI.Point(this.x + this.collider.width / 2, this.y + this.collider.height);
+    if (this.movementState === 'wall-grab-left' || this.movementState === 'wall-grab-right') {
+      this.view.skew.x = 0;
+      this.view.x = 0;
+      this.view.scale.y = 0.7;
+      this.view.scale.x = 1.5;
+      return;
     }
 
-    drawPlayer() {
-        this.view.ellipse(this.collider.width/2, this.collider.height/2, this.collider.width/2, this.collider.height/2);
-        this.view.fill(0xff6633);
-        this.view.stroke({width: 2, color: 0});
-        this.view.circle(this.collider.width / 2, this.collider.width * 0.8, this.collider.width * 0.8);
-        this.view.fill(0xee1144);
-        this.view.stroke({width: 2, color: 0}); 
+    if (this.movementState === 'rolling') {
+      let scale = this.view.scale.y;
+      let dScale = 0.5 - scale;
+      scale = scale + dScale * this.animationSpeed;
+      this.view.scale.y = scale;
+
+      let angle = (this.rollTime / this.maxRollTime) * Math.PI * 2 * Math.sign(-this.vX);
+      this.view.rotation = angle;
+
+      let pivot = new PIXI.Point(this.collider.width / 2, this.collider.height * scale / 2);
+      let offY = this.collider.height * (1 - scale);
+      let dpX = Math.cos(angle) * pivot.x - Math.sin(angle) * pivot.y - pivot.x;
+      let dpY = Math.sin(angle) * pivot.x + Math.cos(angle) * pivot.y - pivot.y;
+      this.view.x = -dpX;
+      this.view.y = -dpY + offY;
+
+      return;
     }
 
-    setMovementState(state: MovementState) {
-        if (state === 'ascending') {
-            Firework.makeExplosion(this.parent, _.defaults(this.getFootPoint(), STEP_PARTICLE));
-        } else if ((this.movementState === 'falling' || this.movementState === 'diving') && state === 'walking') {
-            Firework.makeExplosion(this.parent, _.defaults(this.getFootPoint(), STEP_PARTICLE));
-        }
-        this.movementState = state;
-        GameEvents.ACTIVITY_LOG.publish({slug: 'PLAYER_STATE', text: state});
+    this.view.rotation = 0;
+    this.view.scale.x = 1;
+
+    this.desiredSkew = this.vX * this.skewMult;
+    this.desiredVStretch = 1 + Math.min(Math.abs(this.vY) * this.vStretchMult, this.maxVStretch);
+    if (this.movementState === 'crouching' || this.movementState === 'crawling' || this.movementState === 'climbing-left' || this.movementState === 'climbing-right') {
+      this.desiredVStretch *= 0.5;
+    }
+    if (this.landTime > 0) {
+      this.desiredVStretch *= 0.8;
     }
 
-    public getCollider(state?: MovementState) {
-        state = state || this.movementState;
-        if (state === 'crouching' || state === 'crawling' || state === 'rolling' || state === 'climbing-left' || state === 'climbing-right') {
-            return new PIXI.Rectangle(this.x, this.y + this.collider.height / 2, this.collider.width, this.collider.height / 2);
+    let skew = this.view.skew.x;
+    let dSkew = this.desiredSkew - skew;
+    this.view.skew.x = skew + dSkew * this.animationSpeed;
+    this.view.x = this.view.skew.x;
+
+    let stretch = this.view.scale.y;
+    let dStretch = this.desiredVStretch - stretch;
+    this.view.scale.y = stretch + dStretch * this.animationSpeed;
+    this.view.y = this.collider.height - this.view.scale.y * this.collider.height;
+  }
+
+  updateHands() {
+    this.handsTick = (this.handsTick + 1) % this.handsTime;
+    let dlx = 0;
+    let dly = 0;
+    let drx = 0;
+    let dry = 0;
+
+    switch(this.movementState) {
+      case 'idle': 
+        this.setChildIndex(this.rightHand, 2);
+        this.setChildIndex(this.leftHand, 2);
+        let a = this.handsTick / this.handsTime * Math.PI * 2;
+        let m = this.collider.width * 0.2;
+        dlx = this.collider.width * -0.1 + Math.cos(a) * m;
+        dly = this.collider.height * 0.6 + Math.sin(a) * m;
+        drx = this.collider.width * 1.1 - Math.cos(a + Math.PI) * m;
+        dry = this.collider.height * 0.6 + Math.sin(a + Math.PI) * m;
+        break;
+      case 'walking':
+        this.setChildIndex(this.rightHand, 0);
+        this.setChildIndex(this.leftHand, 0);
+        if (this.vX < 0) {
+          let amt = Math.max(this.vX, -2);
+          dly = this.collider.height * (0.6 - amt * 0.01);
+          dry = this.collider.height * (0.6 - amt * 0.02);
+          dlx = this.collider.width * (-0.1 - amt * 0.1);
+          drx = this.collider.width * (1.1 - amt * 0.5);
+        } else {
+          let amt = Math.min(this.vX, 2);
+          dly = this.collider.height * (0.6 + amt * 0.02);
+          dry = this.collider.height * (0.6 + amt * 0.01);
+          dlx = this.collider.width * (-0.1 - amt * 0.5);
+          drx = this.collider.width * (1.1 - amt * 0.1);
         }
-        return new PIXI.Rectangle(this.x, this.y, this.collider.width, this.collider.height);
+        break;
+
+      default: 
+        this.setChildIndex(this.rightHand, 2);
+        this.setChildIndex(this.leftHand, 2);
+        dly = this.collider.height * (0.6);
+        dry = this.collider.height * (0.6);
+        dlx = this.collider.width * (-0.1);
+        drx = this.collider.width * (1.1);
+        break;
     }
 
-    public getTopCollider() {
-        return new PIXI.Rectangle(this.x, this.y, this.collider.width, this.collider.height / 4);
-    }
-
-    updateView() {
-        this.stepDelay--;
-        if (this.stepDelay <= 0) {
-            this.stepDelay = this.maxStepDelay;
-            if (this.movementState === 'walking' || this.movementState === 'rolling') {
-                // {x: this.x + this.collider.width / 2, y: this.y + this.collider.height}
-                STEP_PARTICLE.x = this.x + this.collider.width / 2;
-                STEP_PARTICLE.y = this.y + this.collider.height;
-
-                Firework.makeExplosion(this.parent, _.defaults(this.getFootPoint(), STEP_PARTICLE));
-            }
-        }
-
-        if (this.movementState === 'jetpacking') {
-            Firework.makeExplosion(Facade.gamePage.canvas.layers[GameCanvas.OBJECTS], _.defaults(this.getMidPoint(), {count: 1, tint: 0xffcc66, mag_min: 1, mag_max: 2}));
-        }
-        
-        if (this.movementState === 'wall-grab-left' || this.movementState === 'wall-grab-right') {
-            this.view.skew.x = 0;
-            this.view.x = 0;
-            this.view.scale.y = 0.7;
-            this.view.scale.x = 1.5;
-            return;
-        }
-
-        if (this.movementState === 'rolling') {
-            let scale = this.view.scale.y;
-            let dScale = 0.5 - scale;
-            scale = scale + dScale * this.animationSpeed;
-            this.view.scale.y = scale;
-
-            let angle = (this.rollTime / this.maxRollTime) * Math.PI * 2 * Math.sign(-this.vX);
-            this.view.rotation = angle;
-
-            let pivot = new PIXI.Point(this.collider.width / 2, this.collider.height * scale / 2);
-            let offY = this.collider.height * (1 - scale);
-            let dpX = Math.cos(angle) * pivot.x - Math.sin(angle) * pivot.y - pivot.x;
-            let dpY = Math.sin(angle) * pivot.x + Math.cos(angle) * pivot.y - pivot.y;
-            this.view.x = -dpX;
-            this.view.y = -dpY + offY;
-
-            return;
-        }
-
-        this.view.rotation = 0;
-        this.view.scale.x = 1;
-
-        this.desiredSkew = this.vX * this.skewMult;
-        this.desiredVStretch = 1 + Math.min(Math.abs(this.vY) * this.vStretchMult, this.maxVStretch);
-        if (this.movementState === 'crouching' || this.movementState === 'crawling' || this.movementState === 'climbing-left' || this.movementState === 'climbing-right') {
-            this.desiredVStretch *= 0.5;
-        }
-        if (this.landTime > 0) {
-            this.desiredVStretch *= 0.8;
-        }
-
-        let skew = this.view.skew.x;
-        let dSkew = this.desiredSkew - skew;
-        this.view.skew.x = skew + dSkew * this.animationSpeed;
-        this.view.x = this.view.skew.x;
-
-        let stretch = this.view.scale.y;
-        let dStretch = this.desiredVStretch - stretch;
-        this.view.scale.y = stretch + dStretch * this.animationSpeed;
-        this.view.y = this.collider.height - this.view.scale.y * this.collider.height;
-    }
+    this.leftHand.x += (dlx - this.leftHand.position.x) * this.animationSpeed;
+    this.leftHand.y += (dly - this.leftHand.position.y) * this.animationSpeed;
+    this.rightHand.x += (drx - this.rightHand.position.x) * this.animationSpeed;
+    this.rightHand.y += (dry - this.rightHand.position.y) * this.animationSpeed;
+  }
 }
 
 const STEP_PARTICLE: IExplosion = {
-    x: 0, y: 0, count: 3, tint: 0x11cc33,
-//     const DExplosion: IExplosion = {
-//   x: 0, y: 0, count: 20,
-//   offRadius: 0,
-//   angle_min: 0, angle_max: Math.PI * 2,
-//   mag_min: 1, mag_max: 3,
-//   fade: 0.06,
-//   size_min: 5, size_max: 9,
-//   tint: 0xcccccc,
-// };
+  x: 0, y: 0, count: 3, tint: 0x11cc33,
+  //     const DExplosion: IExplosion = {
+  //   x: 0, y: 0, count: 20,
+  //   offRadius: 0,
+  //   angle_min: 0, angle_max: Math.PI * 2,
+  //   mag_min: 1, mag_max: 3,
+  //   fade: 0.06,
+  //   size_min: 5, size_max: 9,
+  //   tint: 0xcccccc,
+  // };
 }
 
 export type MovementState = 'idle' | 'walking' | 'ascending' | 'falling' | 'diving' | 'crouching' | 'crawling' | 'rolling' |
-    'wall-grab-left' | 'wall-grab-right' | 'climbing-left' | 'climbing-right' | 'jetpacking';
+  'wall-grab-left' | 'wall-grab-right' | 'climbing-left' | 'climbing-right' | 'jetpacking';
